e96b9c72bf3004d32271e15c3e4a0740c523a443
lebrun26 <lebrun.patrickdev@gmail.com>
2024-08-01 11:00:03 +0200

    installation et mise en place de react router ainsi que les routes pour chaques pages

--- Frontend/ArgentBank/src/App.jsx
+++ Frontend/ArgentBank/src/App.jsx
@@ -1,5 +1,22 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import User from "./pages/User";
+import Error from "./pages/Error";
 
-function App() {}
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/user/:id" element={<User />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
 
 export default App;
