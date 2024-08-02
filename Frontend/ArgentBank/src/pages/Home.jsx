37a3e053b2482bfd57875fae41ee42d2dd506687
lebrun26 <lebrun.patrickdev@gmail.com>
2024-08-02 09:26:10 +0200

    fin developpement footer

--- Frontend/ArgentBank/src/pages/Home.jsx
+++ Frontend/ArgentBank/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 };
