d5364a8580c4354cc544555bfaf14230d3393f2b
lebrun26 <lebrun.patrickdev@gmail.com>
2024-08-02 09:23:22 +0200

    mis en place du css + developpement du header terminer

--- Frontend/ArgentBank/src/components/Header.jsx
+++ Frontend/ArgentBank/src/components/Header.jsx
@@ -4,10 +4,22 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <img
-        src="/images/argentBankLogo.png"
-        alt="logo de l'entreprise ArgentBank"
-      ></img>
+      <nav className="main-nav">
+        <a className="main-nav-logo" href="/">
+          <img
+            className="main-nav-logo-image"
+            src="/images/argentBankLogo.png"
+            alt="logo de l'entreprise ArgentBank"
+          ></img>
+          <h1 className="sr-only">Argent Bank</h1>
+        </a>
+        <div>
+          <a className="main-nav-item" href="/login">
+            <i className="fa fa-user-circle"></i>
+            <span> Sign In</span>
+          </a>
+        </div>
+      </nav>
     </header>
   );
 };
