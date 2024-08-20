5e8e1c788847415c2002ab072a440d22784cfe71
lebrun26 <lebrun.patrickdev@gmail.com>
2024-08-20 10:21:43 +0200

    Mise en place de redux persist

--- Frontend/ArgentBank/src/main.jsx
+++ Frontend/ArgentBank/src/main.jsx
@@ -10,23 +10,46 @@ import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers/index.jsx";
 import { thunk } from "redux-thunk";
 
-//Obtention du conteneur DOM
+// Redux Persist
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // Default is localStorage
+import { PersistGate } from "redux-persist/integration/react";
+
+// Configuration de Redux Persist
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// Application de Redux Persist à votre réducteur racine
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Création du store Redux avec le réducteur persistant
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }).concat(thunk),
+  devTools: true,
+});
+
+// Création du persistor
+const persistor = persistStore(store);
+
+// Obtention du conteneur DOM
 const container = document.getElementById("root");
 
 // Création de la racine
 const root = createRoot(container);
 
-// Création du store Redux
-const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
-  devTools: true,
-});
-
 // Les actions
-
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
